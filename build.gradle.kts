cf6673c9cc35549e9e753b9ce8cd174fe8dadfa5
Robinson <robinson@dorkbox.com>
2022-02-24 00:18:46 +0100

    Updated version

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ object Extras {
     // set for the project
     const val description = "Gradle Plugin to build Vaadin for use by the VaadinUndertow library"
     const val group = "com.dorkbox"
-    const val version = "14.7.5-12"
+    const val version = "14.7.6"
 
     // set as project.ext
     const val name = "Gradle Vaadin"
