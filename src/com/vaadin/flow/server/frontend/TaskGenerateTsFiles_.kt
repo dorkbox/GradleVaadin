131d47eafbc96416f4659f15f060d40d42c7f4d7
Robinson <robinson@dorkbox.com>
2023-01-20 16:49:10 +0100

    exposed json file

--- src/com/vaadin/flow/server/frontend/TaskGenerateTsFiles_.kt
+++ src/com/vaadin/flow/server/frontend/TaskGenerateTsFiles_.kt
@@ -6,6 +6,8 @@ import java.io.File
  * flow-server-2.8.3
  */
 object TaskGenerateTsFiles_ {
+    val TSCONFIG_JSON = "tsconfig.json"
+
     fun execute(npmFolder: File) {
         val genTs = TaskGenerateTsFiles(npmFolder)
         genTs.execute()
