aa3c08370156158cc3fb1004e15cb254d846a319
Robinson <robinson@dorkbox.com>
2026-02-04 16:14:17 +0100

    updated build deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,41 +21,27 @@ plugins {
 
     id("com.gradle.plugin-publish") version "2.0.0"
 
-    id("com.dorkbox.GradleUtils") version "4.4"
+    id("com.dorkbox.GradleUtils") version "4.8"
     id("com.dorkbox.Licensing") version "3.1"
     id("com.dorkbox.VersionUpdate") version "3.0"
 
     kotlin("jvm") version "2.3.0"
 }
 
-object Extras {
-    // set for the project
-    const val description = "Gradle Plugin to build Vaadin for use by the VaadinUndertow library"
-    const val group = "com.dorkbox"
-
-    // the version here ROUGHLY tracks the major/minor version of vaadin!
-    const val version = "14.10.1"
-
-    // set as project.ext
-    const val name = "Gradle Vaadin"
-    const val id = "GradleVaadin"
-    const val vendor = "Dorkbox LLC"
-    const val url = "https://git.dorkbox.com/dorkbox/GradleVaadin"
-    val tags = listOf("vaadin", "undertow")
-
-    const val vaadinUndertowVer = "14.10"
-
-    // These MUST be in lock-step with what the VaadinUndertow launcher defines, otherwise horrific errors can occur.
-    const val vaadinVer = "14.10.1"
-    const val undertowVer = "2.2.21.Final"
-
-    const val vaadinFlowVer = "2.9.2"
-}
 
 ///////////////////////////////
 /////  assign 'Extras'
 ///////////////////////////////
-GradleUtils.load("$projectDir/../../gradle.properties", Extras)
+GradleUtils.load {
+    group = "com.dorkbox"
+    id = "GradleVaadin"
+    description = "Gradle Plugin to build Vaadin for use by the VaadinUndertow library"
+    name = "Gradle Vaadin"
+    // the version here ROUGHLY tracks the major/minor version of vaadin!
+    version = "14.10.1"
+    vendor = "Dorkbox LLC"
+    url = "https://git.dorkbox.com/dorkbox/GradleVaadin"
+}
 GradleUtils.defaults()
 GradleUtils.compileConfiguration(JavaVersion.VERSION_25)
 
@@ -67,23 +53,19 @@ licensing {
     }
 }
 
-sourceSets {
-    main {
-        java {
-            setSrcDirs(listOf("src"))
-
-            // want to include kotlin and java files for the source. 'setSrcDirs' resets includes...
-            // NOTE: if we DO NOT do this, then there will not be any sources in the "plugin sources" jar, as it expects only java
-            include("**/*.kt", "**/*.java")
-        }
-    }
-}
-
 repositories {
     gradlePluginPortal()
 }
 
 dependencies {
+    val vaadinUndertowVer = "14.10"
+
+    // These MUST be in lock-step with what the VaadinUndertow launcher defines, otherwise horrific errors can occur.
+    val vaadinVer = "14.10.1"
+    val undertowVer = "2.2.21.Final"
+
+    val vaadinFlowVer = "2.9.2"
+
     // the kotlin version is taken from the plugin, so it is not necessary to set it here
     compileOnly("org.jetbrains.kotlin:kotlin-gradle-plugin")
 
@@ -94,12 +76,12 @@ dependencies {
     implementation("io.github.classgraph:classgraph:4.8.184")
 
     // implementation("com.vaadin:vaadin:${Extras.vaadinVer}") // NOTE: uncomment for testing ONLY
-    implementation("com.vaadin:flow-server:${Extras.vaadinFlowVer}")
+    implementation("com.vaadin:flow-server:${vaadinFlowVer}")
     implementation("com.vaadin.external.gwt:gwt-elemental:2.8.2.vaadin2")
 
 
     // this is used to announce the version of vaadin to use with the plugin
-    implementation("com.dorkbox:VaadinUndertow:${Extras.vaadinUndertowVer}")
+    implementation("com.dorkbox:VaadinUndertow:${vaadinUndertowVer}")
     implementation("com.dorkbox:Executor:3.14")
     implementation("com.dorkbox:Version:3.1")
 
@@ -110,19 +92,6 @@ tasks.jar.get().apply {
     // include the webpack.*.js files
     from("src") {
         include("com/vaadin/flow/server/frontend/webpack.*.js")
-    }
-
-    manifest {
-        // https://docs.oracle.com/javase/tutorial/deployment/jar/packageman.html
-        attributes["Name"] = Extras.name
-
-        attributes["Specification-Title"] = Extras.name
-        attributes["Specification-Version"] = Extras.version
-        attributes["Specification-Vendor"] = Extras.vendor
-
-        attributes["Implementation-Title"] = "${Extras.group}.${Extras.id}"
-        attributes["Implementation-Version"] = GradleUtils.now()
-        attributes["Implementation-Vendor"] = Extras.vendor
     }
 }
 
@@ -135,12 +104,12 @@ gradlePlugin {
 
     plugins {
         register("GradleVaadin") {
-            id = "${Extras.group}.${Extras.id}"
+            id = Extras.groupAndId
             implementationClass = "dorkbox.gradleVaadin.Vaadin"
             displayName = Extras.name
             description = Extras.description
             version = Extras.version
-            tags.set(Extras.tags)
+            tags.set(listOf("vaadin", "undertow"))
         }
     }
 }
